50f28d2a4a6b85aed2fe308bf007d684bf472bca
wangjun <527760681@qq.com>
2018-07-16 13:48:01 +0800

    added the image data wrote some code to handle the data

--- utils.py
+++ utils.py
@@ -6,9 +6,6 @@ import os
 import random
 import cv2
 import numpy as np
-from PIL import Image
-from keras.preprocessing.image import img_to_array
-
 
 class SingleClassImageHandler():
     def __init__(self, image_path, img_size):
@@ -19,23 +16,40 @@ class SingleClassImageHandler():
         base_image = cv2.imread(self.image_path, flags=cv2.IMREAD_UNCHANGED)
         image = base_image[:, :, 0:3]
         image = np.uint8(image)
-        self.image = image
         return image
 
+
     def random_patch(self):
-        row = self.image.shape[0]
-        col = self.image.shape[1]
+        image = self.load_image()
+        row = image.shape[0]
+        col = image.shape[1]
         r = random.randint(0, row - self.img_size)
         c = random.randint(0, col - self.img_size)
-        sub_image = self.image[r:r + self.img_size, c:c + self.img_size]
+        sub_image = image[r:r + self.img_size, c:c + self.img_size]
 
         return sub_image
 
 data_root = r'.\landuse'
+sub_image_root = r'.\sub_image'
 commercial_image = r'commercial_clip_rgb.jpg'
 Industrial_image = r'Industrial_clip_rgb.jpg'
 Residentia_image = r'Residentia_clip_rgb.jpg'
 Servicepublic_image = r'Servicepublic_clip_rgb.jpg'
 
+patch_num = 200
+img_size = 256
 
-img_size = 255
+image_content = [commercial_image,Industrial_image,Residentia_image,Servicepublic_image]
+
+for image in image_content:
+    handler = SingleClassImageHandler(os.path.join(data_root,image),img_size=img_size)
+
+    class_name = image.split('.')[0]
+    class_path = os.path.join(sub_image_root,class_name)
+    if not os.path.exists(class_path):
+        os.mkdir(class_path) #need to mkdir 'sub_image_root' handly
+
+    for i in range(patch_num):
+        sub_image = handler.random_patch()
+        print('writing down the no.'+str(i)+' image in class '+image)
+        cv2.imwrite(os.path.join(class_path,str(i)+'.jpg'),sub_image)
